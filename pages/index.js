d598f7dd8b2cd7798f349b6238b12218daacb901
Eduardo Cesar Kandalski <31393010+edukandalski@users.noreply.github.com>
2024-09-04 23:34:53 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>O otimismo é a fé daquele que conduz à realização; nada pode ser feito sem esperança. Persista nos seus objetivos!</h1>
+  return (
+    <h1>
+      O otimismo é a fé daquele que conduz à realização; nada pode ser feito sem
+      esperança. Persista nos seus objetivos!
+    </h1>
+  );
 }
 
 export default Home;
